bf1adc2d7d0218d89a5b49d23161e0a33affc85a
henriqueok20 <henriqueok20@gmail.com>
2019-01-03 20:12:10 -0300

    implementada uma validação na tela de login e cadastro

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -24,42 +24,62 @@ export default class Auth extends Component {
         confirmPassword: '',
     }
 
-    signinOrSignup = async () => {
-        if (this.state.stageNew) {
-            try {
-                await axios.post(`${server}/signup`, {
-                    name: this.state.name,
-                    email: this.state.email,
-                    password: this.state.password,
-                    confirmPassword: this.state.confirmPassword,
-                })
-                Alert.alert('Sucesso', 'Usuário cadastrado com sucesso!')
-                this.setState({ stageNew: false })
-            } catch (err) {
-                showError(err)
-            }
-        } else {
-            try {
-                const res = await axios.post(`${server}/signin`, {
-                    email: this.state.email,
-                    password: this.state.password,
+    signin = async () => {
+        try {
+            const res = await axios.post(`${server}/signin`, {
+                email: this.state.email,
+                password: this.state.password,
 
-                })
-                /* a partir de agora, todas as requisições usando o axios
-                terão esse header abaixo, pois dessa forma é possível autenticar as requisições
-                e confirmar se o usuário pode ou não realizar determinada ação. */
-                axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-                /* dando tudo certo, eu chamo a classe anterior e falo pra mudar
-                pra tela Home */
-                this.props.navigation.navigate('Home')
-            } catch (err) {
-                Alert.alert('Erro', 'Falha no login!')
-                //showError(err)
-            }
+            })
+            /* a partir de agora, todas as requisições usando o axios
+            terão esse header abaixo, pois dessa forma é possível autenticar as requisições
+            e confirmar se o usuário pode ou não realizar determinada ação. */
+            axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+            /* dando tudo certo, eu chamo a classe anterior e falo pra mudar
+            pra tela Home */
+            this.props.navigation.navigate('Home')
+        } catch (err) {
+            Alert.alert('Erro', 'Falha no login!')
+            //showError(err)
+        }
+    }
+
+    signup = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword,
+            })
+            Alert.alert('Sucesso', 'Usuário cadastrado com sucesso!')
+            this.setState({ stageNew: false })
+        } catch (err) {
+            showError(err)
+        }
+    }
+
+    signinOrSignup = () => {
+        if (this.state.stageNew) {
+            this.signup()
+        } else {
+            this.signin()
         }
     }
 
     render() {
+        const validations = []
+
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+        if (this.state.stageNew) {
+            validations.push(this.state.name & this.state.name.trim())
+            validations.push(this.state.confirmPassword)
+            validations.push(this.state.password === this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((all, v) => all && v)
+
         return (
             <ImageBackground
                 source={backgroundImage}
@@ -93,7 +113,7 @@ export default class Auth extends Component {
                         value={this.state.email}
                         onChangeText={email => this.setState({ email })}
                     />
-                    
+
                     <Authinput
                         icon='lock'
                         secureTextEntry={true}
@@ -112,9 +132,12 @@ export default class Auth extends Component {
                             style={styles.input}
                             value={this.state.confirmPassword}
                             onChangeText={confirmPassword => this.setState({ confirmPassword })} />}
-                    
-                    <TouchableOpacity onPress={this.signinOrSignup}>
-                        <View style={styles.button}>
+
+                    <TouchableOpacity
+                        disabled={!validForm}
+                        onPress={this.signinOrSignup}>
+                        <View
+                            style={[styles.button, !validForm ? { backgroundColor: '#AAA' } : {}]}>
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Registrar' : 'Entrar'}
                             </Text>
