747f0b9033b5fd6b9e193bb7c398799dd7854a46
henriqueok20 <henriqueok20@gmail.com>
2019-01-03 21:53:38 -0300

    houveram alterações no código para renderizar a tela de forma dinâmica conforme o período escolhido no filtro. agora mostra as tarefas de amanhã, semana e mês e as cores da tela se adaptam a cada período escolhido

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -11,13 +11,17 @@ import {
 import moment from 'moment'
 import axios from 'axios'
 import 'moment/locale/pt-br'
-import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../commonStyles'
 import Task from '../components/Task'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import ActionBUtton from 'react-native-action-button'
 import AddTask from './AddTaks'
 import { server, showError } from '../common'
+
+import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
 
 export default class Agenda extends Component {
 
@@ -89,7 +93,9 @@ export default class Agenda extends Component {
 
     loadTasks = async () => {
         try {
-            const maxDate = moment().format('YYYY-MM-DD 23:59')
+            const maxDate = moment()
+                .add({ days: this.props.daysAhead })
+                .format('YYYY-MM-DD 23:59')
             const res = await axios.get(`${server}/tasks?date=${maxDate}`)
             this.setState({ tasks: res.data }, this.filterTasks)
         } catch (err) {
@@ -98,6 +104,28 @@ export default class Agenda extends Component {
     }
 
     render() {
+        let styleColor = null
+        let image = null
+
+        switch (this.props.daysAhead) {
+            case 0:
+                styleColor = commonStyles.colors.today
+                image = todayImage
+                break
+            case 1:
+                styleColor = commonStyles.colors.tomorrow
+                image = tomorrowImage
+                break
+            case 7:
+                styleColor = commonStyles.colors.week
+                image = weekImage
+                break
+            default:
+                styleColor = commonStyles.colors.month
+                image = monthImage
+                break
+        }
+
         return (
             <View style={styles.container}>
 
@@ -107,9 +135,12 @@ export default class Agenda extends Component {
                     onCancel={() => this.setState({ showAddTask: false })}
                 />
 
-                <ImageBackground source={todayImage} style={styles.background}>
+                <ImageBackground source={image} style={styles.background}>
 
                     <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+                            <Icon name='bars' size={20} color={commonStyles.colors.secundary} />
+                        </TouchableOpacity>
                         <TouchableOpacity onPress={this.toggleFilter}>
                             <Icon
                                 name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
@@ -120,7 +151,7 @@ export default class Agenda extends Component {
                     </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>
-                            Hoje
+                            {this.props.title}
                         </Text>
                         <Text style={styles.subtitle}>
                             {moment().locale('pt-br').format('ddd, D [de] MMMM')}
@@ -142,7 +173,7 @@ export default class Agenda extends Component {
                 </View>
 
                 <ActionBUtton
-                    buttonColor={commonStyles.colors.today}
+                    buttonColor={styleColor}
                     onPress={() => this.setState({ showAddTask: true })}
                 />
 
@@ -184,6 +215,6 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === 'ios' ? 30 : 10,
         marginHorizontal: 20,
         flexDirection: 'row',
-        justifyContent: 'flex-end'
+        justifyContent: 'space-between'
     }
 })
